f516c64b7d32757a32e441d50e74e65b1878153c
zigsong <wldms5764@gmail.com>
2020-12-08 00:08:53 +0900

    feat: login forms & button

--- Myday6/LoginView.swift
+++ Myday6/LoginView.swift
@@ -8,18 +8,51 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
         ZStack {
             BackgroundView()
             
             VStack {
-                Text("MyDay6")
+                Text("MyDay Diary")
                     .font(.system(size: 32, design: .rounded))
                     .foregroundColor(Color.white)
-                    .padding()
-                // Spacer()
+                    .padding(.top, 80)
+                
                 Image("myday_logo")
-                    .padding()
+                    .padding(.top, 60)
+                
+                // Spacer()
+                
+                VStack {
+                    TextField("Email address", text: self.$email)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 15.0).fill(Color.white))
+                        .frame(width: 320, height: nil)
+                        
+                    SecureField("Password", text: self.$password)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 15.0).fill(Color.white))
+                        .frame(width: 320, height: nil)
+                        .padding(.top, 8)
+                            
+                    Button(action: {
+                        
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 50.0).fill(Color.white)
+                                .frame(width: 320, height: 56, alignment: .center)
+                            Text("LOG IN")
+                                .foregroundColor(Color.black)
+                        }
+                    }
+                    .padding(.top, 14)
+                       
+                }
+                .padding(.top, 80)
+                
                 Spacer()
             }
         }
